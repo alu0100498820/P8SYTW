d5d0dee6375fa559e6d1fd9bfe5b4538550dd945
Borja <alu0100498820@ull.edu.es>
2015-12-06 22:09:27 +0000

    Modificando fichero de preguntas multiples

--- models/P_Multiple.js
+++ models/P_Multiple.js
@@ -1,31 +1,31 @@
 var Pregunta = require('../models/pregunta.js');
+var EJS = require('ejs');
 
 function PregSelecMultiple(x, array) {
   Pregunta.call(this);
 
+  this.area_;
   this.pregunta_ = x;
   this.array_ = array;
+  
+  var self = this;
+
+  EJS.renderFile('views/quizes/SelecMultiple.ejs',{array: this.array_}, function(err,html){
+    if(err) throw err;
+    else self.area_ = html;
+  });
 }
 
 PregSelecMultiple.prototype = new Pregunta();
 
 PregSelecMultiple.prototype.constructor = PregSelecMultiple;
 
-PregSelecMultiple.prototype.vista = function(){
-  var vista = [];
-
-  for(var i=0; i<this.array_.length; i++){
-    vista[i] = "<option>" + this.array_[i] + "</option>";
-  }
-
-  vista.unshift("<select name='respuesta[]' size=" + this.array_.length + " multiple>");
-  vista.push("</select>");
-
-  return vista;
-}
-
 PregSelecMultiple.prototype.get_pregunta = function(){
   return this.pregunta_;
+}
+
+PregSelecMultiple.prototype.get_area = function(){
+  return this.area_;
 }
 
 module.exports = PregSelecMultiple;
